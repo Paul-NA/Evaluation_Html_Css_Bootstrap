3dab67c4594c3b7c8d7f7f21c1d56f18260af3d9
Paul N-A <loadingz.com@gmail.com>
2022-05-07 17:18:55 +0200

    Ajout du modal Don et mise à jour du fichier javascript pour prendre en charge les modal

--- Templates/Assoc/Js/script.js
+++ Templates/Assoc/Js/script.js
@@ -12,3 +12,43 @@ function isOnTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+var videoPlayerModal = document.getElementById('videoPlayerModal');
+videoPlayerModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    var recipient = button.getAttribute('data-bs-title');
+    var videoLink = button.getAttribute('data-bs-video');
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    var modalTitle = videoPlayerModal.querySelector('.modal-title');
+    //var modalBodyInput = videoPlayerModal.querySelector('.modal-body input');
+    modalTitle.textContent = 'Replay : ' + recipient;
+    //modalBodyInput.value = recipient;
+    
+    document.getElementById('playerVideo').src = videoLink;
+});
+
+
+var inscriptionConferenceModal = document.getElementById('inscriptionConferenceModal');
+inscriptionConferenceModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget;
+    // Extract info from data-bs-* attributes
+    var recipient = button.getAttribute('data-bs-title');
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    var modalTitle = inscriptionConferenceModal.querySelector('.modal-title');
+    var modalBodyInput = inscriptionConferenceModal.querySelector(".modal-body input[name='conf']");
+    modalTitle.textContent = 'Inscription le ' + recipient;
+    //if(modalBodyInput)
+        modalBodyInput.value = recipient;
+    //else
+    //    alert("fail");
+});
